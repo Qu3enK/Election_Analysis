c0007b7281b8eb460a25c2e5aac5e90819a56ab4
Karr <karr05@yahoo.com>
2022-09-25 10:31:36 -0400

    Added code to id the candidates, set dictionary, count votes

--- PyPoll.py
+++ PyPoll.py
@@ -17,6 +17,15 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter
+total_votes = 0
+
+# Print the candidate name from each row
+candidate_options = []
+
+# Declare the empty dictionary
+candidate_votes = {}
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     
@@ -25,6 +34,28 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    # Print each row in the CSV file
+    for row in file_reader:
 
-    # testing to see if comment is added due to push/pull
+        # 2. Add to the total vote count
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+            # Began tracking that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+# 3. Print the candidate vote dictionary
+print(candidate_votes)
+
